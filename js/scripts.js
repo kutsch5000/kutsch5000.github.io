81c34d4722b48c519909e5c2e385f219e7d4ced1
Steven Kutsch <sckutsch@gmail.com>
2015-10-29 10:33:03 -0700

    Version 1

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+$('a').click(function(){
+	$('.ui.modal').modal('show');
+});
+
+$('.hover1').hide();
+
+$('.clickme1').click(function(){
+	$('.hover1').toggle();
+})
+
+$('.hover2').hide();
+
+$('.clickme2').click(function(){
+	$('.hover2').toggle();
+})
+
+$('.hover3').hide();
+
+$('.clickme3').click(function(){
+	$('.hover3').toggle();
+})
+
+$('.hover4').hide();
+
+$('.clickme4').click(function(){
+	$('.hover4').toggle();
+})
+
+$('.hover5').hide();
+
+$('.clickme5').click(function(){
+	$('.hover5').toggle();
+})
+
+$('.hover6').hide();
+
+$('.clickme6').click(function(){
+	$('.hover6').toggle();
+})
+
+$('.hover7').hide();
+
+$('.clickme7').click(function(){
+	$('.hover7').toggle();
+})
+
+$('.hover8').hide();
+
+$('.clickme8').click(function(){
+	$('.hover8').toggle();
+})
