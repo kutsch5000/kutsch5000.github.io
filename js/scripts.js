d9bfa704b41d4f6f8608fd0a44e6dc73920c72a9
Steven Kutsch <sckutsch@gmail.com>
2015-10-31 15:41:45 -0700

    v 11

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var colors = ["#A51724", "#262261"];
-var logos = ["block", "none"];
+var logos = ["visible", "hidden"];
 var rand = Math.random(); 
 var logocolor = Math.floor(rand * logos.length);
 var randomcolor = Math.floor(rand * colors.length);
@@ -14,13 +14,13 @@ $(document).ready(function () {
 	$(document).ready(function () {
 		$('.red').each(function () {
 			
-        	$('.red').css("display", logos[logocolor]);
+        	$('.red').css("visibility", logos[logocolor]);
     	});
 
    	if (colors[randomcolor] == "#262261"){
    		$(document).ready(function () {
 		$('.blue').each(function () {
-        	$('.blue').css("display", 'block');
+        	$('.blue').css("visibility", 'visible');
     	});
    	});
 };
